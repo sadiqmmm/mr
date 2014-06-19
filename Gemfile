610eca84a77bd846a49ab195cea712d3748e23ec
Mohammed Sadiq <sadiqmmm@gmail.com>
2014-06-19 19:58:04 +0530

    Add Pins association with user

--- Gemfile
+++ Gemfile
@@ -12,6 +12,7 @@ end
 
 group :development, :test do 
   gem 'sqlite3'
+  gem 'pry-rails'
 end
 
 group :production do 
--- app/models/pin.rb
+++ app/models/pin.rb
@@ -2,4 +2,7 @@ class Pin < ActiveRecord::Base
   attr_accessible :description
 
   validates :description, presence: true, length: { maximum: 50}
+  validates :user_id, presence: true 
+
+  belongs_to :user
 end
