5a77d04b10631a9aa869a9ce3b57dcf123ad8021
Mohammed Sadiq <sadiqmmm@gmail.com>
2014-06-19 21:50:07 +0530

    Working style paperclip don't now how it work, just copy paste

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -2,16 +2,23 @@ class Pin < ActiveRecord::Base
   attr_accessible :description, :image
 
   belongs_to :user
-  has_attached_file :image, :style => { :medium => "320x240>", :thumb => "100x100>" }
+   
+  has_attached_file :image,
+    :styles => {
+      :thumb => "100x100#",
+      :small  => "150x150>",
+      :medium => "200x200" }
   
   validates_attachment :image,
   											presence: true,
   											content_type: { content_type: ['image/jpeg', 'image/jpg', 'image/png', 'image/gif'] },
   											size: { less_than: 5.megabytes }
-
+  
 
   validates :description, presence: true, length: { maximum: 50 }
   validates :user_id, presence: true 
+
+  
 
   
 end
