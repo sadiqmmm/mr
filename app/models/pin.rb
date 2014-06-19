cfe95b43de8c015c6726567498b64b0f2784d9a3
Mohammed Sadiq <sadiqmmm@gmail.com>
2014-06-19 22:02:42 +0530

    Add link_to image_tag

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -16,9 +16,5 @@ class Pin < ActiveRecord::Base
   
 
   validates :description, presence: true, length: { maximum: 50 }
-  validates :user_id, presence: true 
-
-  
-
-  
+  validates :user_id, presence: true  
 end
